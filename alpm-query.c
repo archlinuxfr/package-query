c2ce7bfa29b90a80ef508553c7b3a645bcd46729
tuxce <tuxce.net@gmail.com>
2010-03-08 19:02:41 +0100

    More queries.
    Search by name or description.
    Display information on targets.
    Global configuration.

--- alpm-query.c
+++ alpm-query.c
@@ -23,19 +23,69 @@
 #define ROOTDIR "/"
 #define DBPATH "var/lib/pacman"
 #define CONFFILE "etc/pacman.conf"
-#define FORMAT_OUT "%n %v %l"
+#define FORMAT_OUT "%t (%q): %n %v"
+
+
+typedef struct _aq_config 
+{
+	char *myname;
+	unsigned short quiet;
+	unsigned short db_target;
+	unsigned short query;
+	unsigned short information;
+	unsigned short search;
+	char format_out[PATH_MAX];
+	char root_dir[PATH_MAX];
+	char db_path[PATH_MAX];
+	char config_file[PATH_MAX];
+} aq_config;
+
 
 pmdb_t *db;
 pmdb_t *db_local;
 alpm_list_t *dbs=NULL;
-char *myname;
-char format_opt[PATH_MAX] = FORMAT_OUT;
-int quiet = 0;
+
+aq_config config;
+
 extern char *optarg;
 extern int optind;
 
 
-void print_format (const char * format, const char * target, int query, pmpkg_t * pkg_sync, pmpkg_t * pkg_local)
+void init (const char *myname)
+{
+	char *_myname=strdup (myname);
+	config.myname = strdup(basename(_myname));
+	free (_myname);
+	config.quiet = 0;
+	config.db_target = LOCAL;
+	config.query=ALL;
+	config.information = 0;
+	config.search = 0;
+	strcpy (config.root_dir, ROOTDIR);
+	strcpy (config.db_path, DBPATH);
+	strcpy (config.config_file, CONFFILE);
+	strcpy (config.format_out, FORMAT_OUT);
+}
+
+void strins (char *dest, const char *src)
+{
+	char *prov;
+	prov = malloc (sizeof (char) * (strlen (dest) + strlen (src) + 1));
+	strcpy (prov, src);
+	strcat (prov, dest);
+	strcpy (dest, prov);
+	free (prov);
+}
+
+void init_path ()
+{
+	if (config.db_path[0] != '/')
+		strins (config.db_path, config.root_dir);
+	if (config.config_file[0] != '/')
+		strins (config.config_file, config.root_dir);
+}
+
+void print_format (const char * target, int query, pmpkg_t * pkg_sync, pmpkg_t * pkg_local)
 {
 	char *format_cpy;
 	char *ptr;
@@ -43,9 +93,9 @@ void print_format (const char * format, const char * target, int query, pmpkg_t 
 	char *c;
 	char q[8] = {'d','\0', 'c', '\0', 'p', '\0', 'r', '\0'};
 	const char *info;
-	format_cpy = strdup (format);
+	format_cpy = strdup (config.format_out);
 	ptr = format_cpy;
-	end = &(format_cpy[strlen(format)]);
+	end = &(format_cpy[strlen(format_cpy)]);
 	while (c=strchr (ptr, '%'))
 	{
 		if (&(c[1])==end)
@@ -60,10 +110,11 @@ void print_format (const char * format, const char * target, int query, pmpkg_t 
 			info = NULL;
 			switch (c[1])
 			{
+				case 'd': info = alpm_pkg_get_desc (pkg_sync); break;
 				case 'n': info = alpm_pkg_get_name (pkg_sync); break;
 				case 'v': info = alpm_pkg_get_version (pkg_sync); break;
 				case 'l': if (pkg_local) info = alpm_pkg_get_version (pkg_local); break;
-				case 'q': info = &q[(query-1)*2]; break;
+				case 'q': if (query) info = &q[(query-1)*2]; break;
 				case 'r': info = alpm_db_get_name (alpm_pkg_get_db (pkg_sync)); break;
 				case 't': info = target; break;
 				default: ;
@@ -80,19 +131,19 @@ void print_format (const char * format, const char * target, int query, pmpkg_t 
 	printf ("\n");
 }
 
-void print_package (const char * format, const char * target, int query, pmpkg_t * pkg, int local)
+void print_package (const char * target, int query, pmpkg_t * pkg)
 {
-	if (quiet) return;
+	if (config.quiet) return;
 	const char *name;
 	name = alpm_pkg_get_name (pkg);
 	pmpkg_t *pkg_local=NULL;
-	if (!local)
+	if (config.db_target != LOCAL)
 	{
 		pkg_local = alpm_db_get_pkg(db_local, name);
-		print_format (format, target, query, pkg, pkg_local);
+		print_format (target, query, pkg, pkg_local);
 	}
 	else
-		print_format (format, target, query, pkg, pkg);
+		print_format (target, query, pkg, pkg);
 }
 static char *resolve_path(const char* file)
 {
@@ -121,6 +172,105 @@ const char *ret_depname (void *data)
 	return alpm_dep_get_name (data);
 }
 
+char *concat_str_list (alpm_list_t *l)
+{
+	char *ret;
+	int len=0;
+	alpm_list_t *i;
+	if (!l)
+	{
+		ret = (char *) malloc (sizeof (char) * 2);
+		strcpy (ret, "-");
+	}
+	else
+	{
+		for(i = l; i; i = alpm_list_next(i)) 
+		{
+			len += strlen (alpm_list_getdata (i)) + 1;
+		}
+		if (len)
+		{
+			ret = (char *) malloc (sizeof (char) * len);
+			strcpy (ret, "");
+			for(i = l; i; i = alpm_list_next(i)) 
+			{
+				strcat (ret, alpm_list_getdata (i));
+				strcat (ret, " ");
+			}
+		}
+		else
+		{
+			ret = (char *) malloc (sizeof (char) * 2);
+			strcpy (ret, "+");			
+		}
+	}
+	return ret;
+}
+
+			
+int search_pkg (alpm_list_t *targets)
+{
+	int ret=0;
+	int found;
+	const char *compare_to_name;
+	const char *compare_to_desc;
+	alpm_list_t *t;
+	alpm_list_t *i;
+
+	for(i = alpm_db_get_pkgcache(db); i; i = alpm_list_next(i)) 
+	{
+		pmpkg_t *info = alpm_list_getdata(i);
+		compare_to_name = alpm_pkg_get_name(info);
+		compare_to_desc = alpm_pkg_get_desc(info);
+		found = 0;
+		for(t = targets; t; t = alpm_list_next(t)) 
+		{
+			const char *target = alpm_list_getdata(t);
+			if (strcasestr (compare_to_name, target) != NULL)
+			{
+				found++;
+				continue;
+			}
+			if (compare_to_desc == NULL)
+			{
+				continue;
+			}
+			if (strcasestr (compare_to_desc, target) != NULL)
+			{
+				found++;
+				continue;
+			}
+			found = 0;
+		}
+		if (found==alpm_list_count(targets))
+		{	
+			char *ts;
+			ret++;
+			ts = concat_str_list (targets);
+			print_package (ts, 0, info);
+			free(ts);
+		}
+	}
+	return ret;
+}
+
+int check_pkg_name (alpm_list_t *targets)
+{
+	int ret=0;
+	alpm_list_t *t;
+	pmpkg_t *pkg_found;
+	for(t = targets; t; t = alpm_list_next(t)) 
+	{
+		char *pkg_name = alpm_list_getdata(t);
+		pkg_found = alpm_db_get_pkg (db, pkg_name);
+		if (pkg_found != NULL)
+		{
+			ret++;
+			print_package (pkg_name, 0, pkg_found);
+		}
+	}
+	return ret;
+}
 
 int check_pkg (alpm_list_t *targets, int query, alpm_list_t *(*f)(pmpkg_t *), 
 	const char *(*g) (void *))
@@ -139,11 +289,11 @@ int check_pkg (alpm_list_t *targets, int query, alpm_list_t *(*f)(pmpkg_t *),
 			const char *compare_to = g(alpm_list_getdata(j));
 			for(t = targets; t; t = alpm_list_next(t)) 
 			{
-				char *pkg = alpm_list_getdata(t);
-				if (strcmp (pkg, compare_to) == 0)
+				char *pkg_name = alpm_list_getdata(t);
+				if (strcmp (pkg_name, compare_to) == 0)
 				{
 					ret++;
-					print_package (format_opt, pkg, query, info, 0);
+					print_package (pkg_name, query, info);
 					found = 1;
 				}
 			}
@@ -235,24 +385,33 @@ int parse_config (const char * config_file)
 			
 	
 
-void usage (const char * prog)
+void usage ()
 {
 	fprintf(stderr, "Query alpm database\n");
-	fprintf(stderr, "Usage: %s [options] [targets ...]\n", prog);
+	fprintf(stderr, "Usage: %s [options] [targets ...]\n", config.myname);
 	fprintf(stderr, "\nwhere options include:");
 	fprintf(stderr, "\n\t-c <configuration file> : default %s", CONFFILE);
 	fprintf(stderr, "\n\t-b <database path> : default %s", DBPATH);
 	fprintf(stderr, "\n\t-f <format> : default %s", FORMAT_OUT);
-	fprintf(stderr, "\n\t-l search in local database");
+	fprintf(stderr, "\n\t-i <information> show information on targets");
+	fprintf(stderr, "\n\t-q quiet");
+	fprintf(stderr, "\n\t-Q search in local database");
 	fprintf(stderr, "\n\t-r <root path> : default %s", ROOTDIR);
-	fprintf(stderr, "\n\t-s <repos separated by comma> search in sync database");
-	fprintf(stderr, "\n\t-t query_type");
+	fprintf(stderr, "\n\t-s search");
+	fprintf(stderr, "\n\t-S search in sync database");
+	fprintf(stderr, "\n\t-t query type");
 	fprintf(stderr, "\n\t-h this help");
+	fprintf(stderr, "\n\ninformation type: (one information by line)");
+	fprintf(stderr, "\n\tnone: display suits format");
+	fprintf(stderr, "\n\tdepends: depends");
+	fprintf(stderr, "\n\tconflicts: conflicts");
+	fprintf(stderr, "\n\tprovides: provides");
+	fprintf(stderr, "\n\treplaces: replaces");
 	fprintf(stderr, "\n\nquery type:");
-	fprintf(stderr, "\n\tdepends: depends on target");
-	fprintf(stderr, "\n\tconflicts: conflicts with target");
-	fprintf(stderr, "\n\tprovides: provide target");
-	fprintf(stderr, "\n\treplaces: replace target");
+	fprintf(stderr, "\n\tdepends: depends on one of target");
+	fprintf(stderr, "\n\tconflicts: conflicts with one of target");
+	fprintf(stderr, "\n\tprovides: provides one of target");
+	fprintf(stderr, "\n\treplaces: replaces one of target");
 	fprintf(stderr, "\n\nformat:");
 	fprintf(stderr, "\n\tl: local version");
 	fprintf(stderr, "\n\tn: name");
@@ -261,7 +420,6 @@ void usage (const char * prog)
 	fprintf(stderr, "\n\tt: target");
 	fprintf(stderr, "\n\tv: sync version");
 	fprintf(stderr, "\n");
-	free (myname);
 	exit (1);
 }
 	
@@ -270,55 +428,54 @@ int main (int argc, char **argv)
 	int ret=0;
 	alpm_list_t *targets=NULL;
 	alpm_list_t *t;
-	char root_dir[PATH_MAX], 
-		db_path[PATH_MAX]="",
-		config_file[PATH_MAX]="";
 
-	myname = strdup(argv[0]);
-	strcpy (root_dir, ROOTDIR);
+	init (argv[0]);
 
 	int opt;
-	int db_target=LOCAL;
-	int query=ALL;
-	//char *
-	while ((opt = getopt (argc, argv, "c:b:f:hlqr:st:")) != -1) 
+	while ((opt = getopt (argc, argv, "c:b:f:hi::Qqr:Sst:")) != -1) 
 	{
 		switch (opt) 
 		{
 			case 'c':
-				strcpy (config_file, optarg);
+				strcpy (config.config_file, optarg);
 				break;
 			case 'b':
-				strcpy (db_path, optarg);
+				strcpy (config.db_path, optarg);
 				break;
 			case 'f':
-				strcpy (format_opt, optarg);
+				strcpy (config.format_out, optarg);
 				break;
-			case 'l':
-				db_target = LOCAL;
+			case 'i':
+				config.information = 1;
+				break;
+			case 'Q':
+				config.db_target = LOCAL;
 				break;
 			case 'q':
-				quiet = 1;
+				config.quiet = 1;
 				break;
 			case 'r':
-				strcpy (root_dir, optarg);
+				strcpy(config.root_dir, optarg);
 				break;
 			case 's':
-				db_target = SYNC;
+				config.search = 1;
+				break;
+			case 'S':
+				config.db_target = SYNC;
 				break;
 			case 't':
 				if (strcmp (optarg, "depends")==0)
-					query = DEPENDS;
+					config.query = DEPENDS;
 				else if (strcmp (optarg, "conflicts")==0)
-					query = CONFLICTS;
+					config.query = CONFLICTS;
 				else if (strcmp (optarg, "provides")==0)
-					query = PROVIDES;
+					config.query = PROVIDES;
 				else if (strcmp (optarg, "replaces")==0)
-					query = REPLACES;
+					config.query = REPLACES;
 				break;
 			case 'h':
 			default: /* '?' */
-				usage (basename(myname));
+				usage ();
 		}
 	}
 	int i;
@@ -329,45 +486,47 @@ int main (int argc, char **argv)
 	if (targets == NULL)
 	{
 		fprintf(stderr, "no targets specified.\n");
-		usage(basename(myname));
+		usage();
 	}
+	init_path ();
 	alpm_initialize();
-	alpm_option_set_root(root_dir);
-	if (db_path[0] == '\0')
-	{
-		strcpy (db_path, root_dir);
-		strcat (db_path, DBPATH);
-	}
-	if (config_file[0] == '\0')
-	{
-		strcpy (config_file, root_dir);
-		strcat (config_file, CONFFILE);
-	}
-	alpm_option_set_dbpath(db_path);
+	alpm_option_set_root(config.root_dir);
+	alpm_option_set_dbpath(config.db_path);
 
 	db_local = alpm_db_register_local();
-	if (db_target != LOCAL)
-		parse_config (config_file);
+	if (config.db_target != LOCAL)
+		parse_config (config.config_file);
 	else
 		dbs = alpm_list_add(dbs, db_local);
 	for(t = dbs; t; t = alpm_list_next(t))
 	{
 		db = alpm_list_getdata(t);
-		switch (query)
+		if (config.information)
 		{
-			case ALL:
-			case DEPENDS: 
-				ret += check_pkg (targets, DEPENDS, alpm_pkg_get_depends, ret_depname);
-				if (query != ALL) break;
-			case CONFLICTS: 
-				ret += check_pkg (targets, CONFLICTS, alpm_pkg_get_conflicts, ret_char);
-				if (query != ALL) break;
-			case PROVIDES: 
-				ret += check_pkg (targets, PROVIDES, alpm_pkg_get_provides, ret_char);
-				if (query != ALL) break;
-			case REPLACES: 
-				ret += check_pkg (targets, REPLACES, alpm_pkg_get_replaces, ret_char);
-				if (query != ALL) break;
+			ret += check_pkg_name (targets);
+		}
+		else if (config.search)
+		{
+			ret += search_pkg (targets);
+		}
+		else
+		{
+			switch (config.query)
+			{
+				case ALL:
+				case DEPENDS: 
+					ret += check_pkg (targets, DEPENDS, alpm_pkg_get_depends, ret_depname);
+					if (config.query != ALL) break;
+				case CONFLICTS: 
+					ret += check_pkg (targets, CONFLICTS, alpm_pkg_get_conflicts, ret_char);
+					if (config.query != ALL) break;
+				case PROVIDES: 
+					ret += check_pkg (targets, PROVIDES, alpm_pkg_get_provides, ret_char);
+					if (config.query != ALL) break;
+				case REPLACES: 
+					ret += check_pkg (targets, REPLACES, alpm_pkg_get_replaces, ret_char);
+					if (config.query != ALL) break;
+			}
 		}
 	}
 	FREELIST(dbs);
