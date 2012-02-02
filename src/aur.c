1c8f8f72df423f9693b4f38e11cc4af4fe97b9a9
tuxce <tuxce.net@gmail.com>
2012-02-02 15:03:04 +0100

    Copyright 2012

--- src/aur.c
+++ src/aur.c
@@ -1,7 +1,7 @@
 /*
  *  aur.c
  *
- *  Copyright (c) 2010-2011 Tuxce <tuxce.net@gmail.com>
+ *  Copyright (c) 2010-2012 Tuxce <tuxce.net@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
