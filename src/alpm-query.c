501f1f187bb3c3609d191be00aeae0d99636cf7a
Denis Ollier <larchunix@users.noreply.github.com>
2018-05-31 18:30:04 +0200

    Add support for local file package make dependencies (%M)
    Supported by libalpm since https://git.archlinux.org/pacman.git/commit/?id=0994893
    but only for package alpm backend.
    
    It is not implemented yet by localdb and syncdb backends.
    
    Bump libalpm requirement to 11.0.0

--- src/alpm-query.c
+++ src/alpm-query.c
@@ -595,6 +595,13 @@ const char *alpm_pkg_get_str (const void *p, unsigned char c)
 		case 'm':
 			info = (char *) alpm_pkg_get_packager (pkg);
 			break;
+		case 'M':
+			if (alpm_pkg_get_origin (pkg) != ALPM_PKG_FROM_FILE) {
+				return NULL;
+			}
+			info = concat_dep_list (alpm_pkg_get_makedepends (pkg));
+			free_info = true;
+			break;
 		case 'n':
 			info = (char *) alpm_pkg_get_name (pkg);
 			break;
