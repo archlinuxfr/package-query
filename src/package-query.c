629cf78f9436e3da96da0df37c72daeb0f34adca
Denis Ollier <larchunix@users.noreply.github.com>
2018-06-11 19:21:55 +0200

    Add meson build system configuration files
    Build steps become:
    
        > meson setup --prefix=/usr --localstatedir=/var --sysconfdir=/etc build/
        > ninja -C build/
        > ninja -C build/ install

--- src/package-query.c
+++ src/package-query.c
@@ -17,7 +17,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <config.h>
+#include "config.h"
 #if defined(GIT_VERSION)
 #undef PACKAGE_VERSION
 #define PACKAGE_VERSION GIT_VERSION
